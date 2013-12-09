cf1a8469856ce2c1968765b01760085a0c81be2c
millermedeiros <miller@millermedeiros.com>
2013-12-09 15:41:07 -0200

    add support for MultipleVariableDeclaration

--- test/compare/default-out.js
+++ test/compare/default-out.js
@@ -83,3 +83,23 @@ try {
   bar()
 }
 
+
+
+// Vars
+
+var a,
+  b = true,
+  c = 3,
+  d = false;
+
+var asi = true
+var amet = qwe();
+
+
+// comments
+var
+  // foo var
+  foo,
+  // bar variable
+  bar = 'dolor amet';
+
